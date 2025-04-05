ee0fb8f5db40bc1228c8e0b4fc0963dcc9ca2a98
smartitsoloutions <137606026+smartitsoloutions@users.noreply.github.com>
2025-04-05 07:23:10 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -11,7 +11,7 @@ public class HelloWorld {
         System.out.println("Hello, " + name + "!");
     }
 }
-Oblect env
+Object env
  public static void greet(String name) {
-        System.out.println("Hello, " + nfull ame + "!");
+        System.out.println("Hello, " + full name + "!");
     }
