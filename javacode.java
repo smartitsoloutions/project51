809d778d48a3846b5188988305f55306d21596d1
smartitsoloutions <prasads128312@gmail.com>
2025-04-05 07:07:19 +0530

    this is my first project 51 commit

---
+++ javacode.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    // Main method: entry point of the Java program
+    public static void main(String[] args) {
+        // Call a method to print a greeting
+        greet("World");
+    }
+
+    // A simple method to print a greeting
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
